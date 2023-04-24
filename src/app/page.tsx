fead2eb0d3935c9393d0113c674f5919fea6105e
samyCode3 <sadeniyi016@gmail.com>
2023-04-24 03:01:29 +0100

    Revert "Landingpage Built"
    This reverts commit 54db94ea010d7031ec43b6b1b91a7bf2d23d0c11.

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import LandingPage from '../components/templates/LandingPage/index'
-
-export default function Home() {
-  return (
-    <LandingPage />
-  );
-}
--- src/components/UI/atoms/Links/FooterLink/index.tsx
+++
@@ -1,8 +0,0 @@
-import react from 'react'
-const FooterLinks = (props: {name: string}) => {
-    return(
-        <a className='text-lg text-[#5e6282] leading-10 font-normal'  href="http://" target="_blank" rel="noopener noreferrer">{props.name}</a>
-    )
-}
-
-export default FooterLinks
